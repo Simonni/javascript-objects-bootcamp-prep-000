f22d48cc932109c5d04412aee5abb345d2ce4db3
Simon Kiflay <szkiflay@gmail.com>
2018-08-14 18:19:37 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,10 +1,10 @@
 // creating an Object
-var playlist = {'Micheal Jackson': 'Thriller', 'Whitney Houtson': 'The Body G'};
+var playlist = {'Micheal Jackson': 'Thriller', 'Whitney Houtson': 'The Bodyguard'};
 function updatePlaylist(playlist, artist, song){
-  return playlist.artistName, playlist.songTitle;
+  return playlist.artist, playlist.song;
   
 }
-function removeFromPlaylist(playlist, artistName){
-  delete playlist.artistName;
+function removeFromPlaylist(playlist, artist){
+  delete playlist.artist;
   return playlist;
 }
