c0b5baf2de1cda94402036d115eee61dd7d9b1aa
Simon Kiflay <szkiflay@gmail.com>
2018-08-14 17:55:58 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,10 @@
+// creating an Object
+var playlist = {artist: "artistName", song:"songTitle"};
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist.artistName, playlist.artistName;
+  return playlist;
+}
+function removeFromPlaylist(playlist, artistName){
+  delete playlist.artistName;
+  return playlist;
+}
